413a5ef3472578635be945e302e303d50143542f
Phạm Thành An <93148075+ancol-etylic@users.noreply.github.com>
2022-07-05 20:44:24 +0700

    Update C01007-PHEP TOAN CO BAN 2.cpp

--- C01007-PHEP TOAN CO BAN 2.cpp
+++ C01007-PHEP TOAN CO BAN 2.cpp
@@ -3,8 +3,5 @@
 int main(){
 	long long  a,b;
 	scanf("%lld %lld",&a,&b);
-	if(b>0){
 		printf("%lld \n%lld \n%lld \n%lld \n%lld \n%.2llf \n", a+b, a-b, a*b, a/b,a%b, (double)a/b);
-	}
-	else printf("0\n");
 }
