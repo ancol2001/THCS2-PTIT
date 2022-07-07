a575202c75f73fdfa9ac10d262d7509bbe2701bf
Phạm Thành An <93148075+ancol-etylic@users.noreply.github.com>
2022-07-07 07:37:12 +0700

    Delete C01007-PHEP TOAN CO BAN 2.cpp

--- C01007-PHEP TOAN CO BAN 2.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-	long long  a,b;
-	scanf("%lld %lld",&a,&b);
-		printf("%lld \n%lld \n%lld \n%lld \n%lld \n%.2llf \n", a+b, a-b, a*b, a/b,a%b, (double)a/b);
-}
