0dbd2932d160ed53e4babf80ba9dc64b496cf7c1
Phạm Thành An <93148075+ancol-etylic@users.noreply.github.com>
2022-07-10 16:29:09 +0700

    Add files via upload

--- C03003-LIET KE N SO NGUYEN TO DAU TIEN.cpp
+++ C03003-LIET KE N SO NGUYEN TO DAU TIEN.cpp
@@ -1,25 +1,24 @@
 #include<stdio.h>
 #include<math.h>
-int snto(long long a){
-	int dem=0;
-		for (int j = 1; j <= sqrt(a); j++)
-		{
-			if(a%j==0){dem++;}
-		}
-		if (dem==1)
-		{
-			return 1;
-		}
+int ktsnt (long long a) {
+    if (a<=1) return 0;
+    else {
+        for (long long i=2; i<=sqrt (a); i++) {
+            if (a%i==0) return 0;
+        }
+    }
+    return 1;
 }
-int main(){
-	int n;
-	scanf("%d",&n);
-		for (long long i = 2; n>0 ; i++)
-		{
-			if (snto(i)==1)
-			{
-				printf("%lld\n",i);
-				n--;
-			}
-		}
-	}
+int main() {
+ 	long long n;
+    scanf("%lld",&n); 
+    long long i = 2;   
+    while (n>0) {
+        if (ktsnt(i)==1) {
+            printf("%lld\n", i);
+            n--;
+        }
+        i++;
+    }
+    return 0;
+}
